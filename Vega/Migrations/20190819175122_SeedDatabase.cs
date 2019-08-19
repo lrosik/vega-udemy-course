c2d72edaabda570d0bccdde4f8f8aa82d52bd1be
Łukasz Rosik <lukiro96@gmail.com>
2019-08-19 20:05:12 +0200

    added data in OnModelCreating, SeedDatabase migration

---
+++ Vega/Migrations/20190819175122_SeedDatabase.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vega.Migrations
+{
+    public partial class SeedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Make",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Skoda" },
+                    { 2, "Opel" },
+                    { 3, "Audi" },
+                    { 4, "Toyota" },
+                    { 5, "Volkswagen" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Models",
+                columns: new[] { "Id", "MakeId", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Fabia" },
+                    { 2, 1, "Octavia" },
+                    { 3, 1, "Rapid" },
+                    { 4, 2, "Insignia" },
+                    { 5, 2, "Astra" },
+                    { 6, 2, "Corsa" },
+                    { 7, 3, "A4" },
+                    { 8, 3, "A6" },
+                    { 9, 4, "Yaris" },
+                    { 10, 4, "Corolla" },
+                    { 11, 4, "Prius" },
+                    { 12, 5, "Polo" },
+                    { 13, 5, "Golf" },
+                    { 14, 5, "Passat" },
+                    { 15, 5, "Tiguan" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Models",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Models",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Models",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Models",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Models",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Models",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Models",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Models",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Models",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Models",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Models",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Models",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Models",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Models",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Models",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Make",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Make",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Make",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Make",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Make",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
--- Vega/Models/VegaDbContext.cs
+++ Vega/Models/VegaDbContext.cs
@@ -11,5 +11,36 @@ namespace Vega.Models
         }
 
         public DbSet<Model> Models { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Make>().HasData(
+                new Make { Id = 1, Name = "Skoda" },
+                new Make { Id = 2, Name = "Opel" },
+                new Make { Id = 3, Name = "Audi" },
+                new Make { Id = 4, Name = "Toyota" },
+                new Make { Id = 5, Name = "Volkswagen" }
+            );
+
+            modelBuilder.Entity<Model>().HasData(
+                new Model { Id = 1, Name = "Fabia", MakeId = 1 },
+                new Model { Id = 2, Name = "Octavia", MakeId = 1 },
+                new Model { Id = 3, Name = "Rapid", MakeId = 1 },
+                new Model { Id = 4, Name = "Insignia", MakeId = 2 },
+                new Model { Id = 5, Name = "Astra", MakeId = 2 },
+                new Model { Id = 6, Name = "Corsa", MakeId = 2 },
+                new Model { Id = 7, Name = "A4", MakeId = 3 },
+                new Model { Id = 8, Name = "A6", MakeId = 3 },
+                new Model { Id = 9, Name = "Yaris", MakeId = 4 },
+                new Model { Id = 10, Name = "Corolla", MakeId = 4 },
+                new Model { Id = 11, Name = "Prius", MakeId = 4 },
+                new Model { Id = 12, Name = "Polo", MakeId = 5 },
+                new Model { Id = 13, Name = "Golf", MakeId = 5 },
+                new Model { Id = 14, Name = "Passat", MakeId = 5 },
+                new Model { Id = 15, Name = "Tiguan", MakeId = 5 }
+            );
+        }
     }
 }
